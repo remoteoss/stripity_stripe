9c6ab0a6ec65fe1a70d46c5c84d5d7cd2c469183
Victor Yuji Muramoto <victor.muramoto@remote.com>
2022-09-01 17:06:35 +0200

    re-bump

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,5 @@ import Config
 if File.exists?("config/config.secret.exs") do
   import_config "config.secret.exs"
 end
+
+config :stripity_stripe, api_key: "sk_test_51KVeNwDmYNhhsoWQdQHyHnAIAnJ5DGnLnXdntvgcxWNl1QmyycUtdsqe6wQKLPwqmymfVcoDcNBsmK8qSYZ4WECD00gRw9JlMB"
--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
 config :logger, level: :debug
+
+config :stripity_stripe, api_key: "sk_test_51KVeNwDmYNhhsoWQdQHyHnAIAnJ5DGnLnXdntvgcxWNl1QmyycUtdsqe6wQKLPwqmymfVcoDcNBsmK8qSYZ4WECD00gRw9JlMB"
