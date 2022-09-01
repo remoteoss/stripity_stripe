6bd118d87a01459c55191fc86f31df8847d461a4
Victor Yuji Muramoto <victor.muramoto@remote.com>
2022-09-01 17:10:54 +0200

    rebump

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,3 @@
 use Mix.Config
 
 config :logger, level: :debug
-
-config :stripity_stripe, api_key: "sk_test_51KVeNwDmYNhhsoWQdQHyHnAIAnJ5DGnLnXdntvgcxWNl1QmyycUtdsqe6wQKLPwqmymfVcoDcNBsmK8qSYZ4WECD00gRw9JlMB"
